f78d349c5058e9c17faa6cfd9cff3bd72887963e
Steve-Kim <60303689+mircocky@users.noreply.github.com>
2023-03-15 10:17:13 +1100

    Add files via upload

---
+++ pages/booking.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import React ,{ useState } from 'react'
+
+
+const Booking: NextPage = () => {
+
+    const [bookNum, setBookNum] = useState<string>('1002')
+
+  return (
+   <div>
+    <h1>Booking Page {bookNum} </h1>
+    <div className="text-red-500 text-3xl font-bold underline">Hello</div>
+   </div>
+  )
+}
+
+export default Booking
